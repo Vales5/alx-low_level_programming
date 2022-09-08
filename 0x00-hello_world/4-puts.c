0bd586a0e1f88dace16657748e7c81e7d5a4b45f
Vales5 <oliseogbechi@gmail.com>
2022-09-08 15:12:14 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
  *
  *   *
  *
- *    * Description: A C program that prints with put function
+ *    * Return: always 0
  *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
+ *     */
 
 int main(void)
 
