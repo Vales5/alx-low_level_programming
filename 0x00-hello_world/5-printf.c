eebb0ced4eb8804b8e33fe0e62b47398fe5af841
Vales5 <oliseogbechi@gmail.com>
2022-09-08 15:53:03 -0700

    work abeg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - Entry point
  *
  *   *
@@ -10,11 +9,9 @@
  */
 
 int main(void)
-
 {
 
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
-
 }
