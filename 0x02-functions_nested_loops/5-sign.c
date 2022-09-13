6b973c19a94991f3d8601260e00c199d1ffdd1a8
Vales5 <oliseogbechi@gmail.com>
2022-09-13 11:57:36 -0700

    ght

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include"main"
+
+/**
+ * print_sign - main function
+ *
+ * @n: character to compare
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		return (-1);
+	}
+}
