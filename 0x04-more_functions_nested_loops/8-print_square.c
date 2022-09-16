7475c06b37abf5c5a3c8a2fbe8f2f3721f0f0e84
Vales5 <oliseogbechi@gmail.com>
2022-09-15 17:35:53 -0700

    at this point

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,11 @@ void print_square(int size)
 	int row, column;
 
 	if (size <= 0)
-		_putchar('\n';
+		_putchar('\n');
 
-	for (row = 1; row <= size; ++row)
+	for (row = 1; row < size; row++)
 	{
-		for (column = 1; column <= (size); ++column)
+		for (column = 1; column <= size; column++)
 		{
 			_putchar('#');
 		}
